649ce1670474a0559be52c4a827ac4cb34ad6477
unniisme <122001019@smail.iitpkd.ac.in>
2022-11-23 11:59:31 +0530

    23/11/22

--- Rust/Intro.md
+++ Rust/Intro.md
@@ -8,7 +8,8 @@ Rust is a low level programming language
 Some properties
 - ; is a separator
 - variables are immutable by default
-```
+
+```rust
 //Immutable variable
 let a = 12
 a = 14 //Throws error
@@ -20,7 +21,8 @@ b = 15 //Works fine
 ---
 To run test code in `rust_test/src/main.rs`  
 Provided you have rustup (and therefor cargo) installed
-```
+
+```shell
 $ cd rust_test
 $ cargo run
 ```
@@ -55,9 +57,43 @@ Shallow copy is useful for precise control, and when garbage collection has to b
 Rust expects no garbage collection by default.  
 It can have either Pure values, or resources kept in a `Box`.  
 
-Normal variables (say u32) has copy semantics.  
-Boxes (say Box\<u32>) has move sematics
+Normal variables (say u32) have copy semantics.  
+Boxes (say Box\<u32>) have move sematics
 
 Multiple pointers cannot point to the same object.
 
 Memory gets deallocated (for box) if owner does not exist any longer.
+
+### Defining types
+
+1) By default, a defined type follows move semantics
+
+```rust
+struct Rect
+{
+        pub length:u32
+        pub breadth:u32 
+}
+```
+
+2) If a type T has the copy trait then it is governed by the copy semantics instead of move semantics.  
+For the same, a copy trait for the type has to be implemented.
+
+```rust
+// A general trait
+trait Tname
+{
+        fn foo(&self) -> u32;
+        fn bar(&self, x:u32) -> u32;
+}
+
+// trait Copy:Clone // is pre defined
+// See <https://doc.rust-lang.org/std/clone/trait.Clone.html>
+impl Clone for Rect
+{
+        fn clone(&self) ->  Rect{ *self }
+}
+
+/// See <https://doc.rust-lang.org/std/marker/trait.Copy.html>
+impl Copy for Rect {}
+```
--- Rust/rust_test/src/main.rs
+++ Rust/rust_test/src/main.rs
@@ -2,10 +2,66 @@
 // Run with cargo run in ..
 
 //Function to test memory allocation
-fn foo(u:Box<u32>)
+fn foo(_u:Box<u32>)
 {
     println!("In foo");
 }
+
+// New datatype, with move semantics
+struct Rectangle
+{
+    pub length:u32,
+    pub breadth:u32 
+}
+
+fn rect_area(rect:Rectangle) -> u32
+{
+    return rect.length * rect.breadth;
+}
+
+
+// New datatype, we'll also define traits for it
+struct Cuboid
+{
+    pub length:u32,
+    pub breadth:u32,
+    pub height:u32
+}
+
+pub trait Obj3D
+{
+    fn volume(&self) -> u32;
+
+    fn to_string(&self) -> String;
+}
+
+fn cuboid_area(cub:Cuboid) -> u32
+{
+    return 2*(cub.length*cub.breadth + cub.breadth*cub.height + cub.height*cub.length);
+}
+
+impl Obj3D for Cuboid
+{
+    fn volume(&self) -> u32
+    {
+        return self.length*self.breadth*self.height;
+    }
+
+    fn to_string(&self) -> String
+    {
+        return format!("length:{}\nbreadth:{}\nheight:{}", self.length, self.breadth, self.height);
+    }
+}
+
+// Copy semantics for cuboid
+impl Clone for Cuboid
+{
+    fn clone(&self) ->  Cuboid{ *self }
+}
+
+impl Copy for Cuboid {}
+
+
 
 fn main() {
     println!("A simple Testing program in rust");
@@ -27,26 +83,24 @@ fn main() {
     foo(b);
     //println!("Boxed variable b is {}", b); //This line throws an error as b was deallocated by foo()
 
-     
+    println!("");
+    println!("--Types--");
+    // -----------Types------------
+    let rect:Rectangle = Rectangle{length:10, breadth:12};
+    println!("rect.length : {}", rect.length);
+    println!("rect.breadth : {}", rect.breadth);
+    println!("RectArea(rect) : {}", rect_area(rect));
+    println!("rect has been moved and possibly garbage collected. :(");
 
-    /*
-    //loops
-    let mut count:u32 = 0;
-    loop
-    {
-        if count==0
-        {
-            println!("Entering a loop");
-        }
-        println!("Loop number {}", count);
+    //println!("RectArea(rect) : {}", rect_area(rect)); //This line will throw an error as rect was moved during rect_area
+    
+    println!("");
+    let cube:Cuboid = Cuboid{length:10, breadth:10, height:10};
+    println!("cube.to_string()\n{}", cube.to_string());
+    println!("cube.volume() : {}", cube.volume());
 
-        count=count+1;
+    println!("cuboid_area(cube) : {}", cuboid_area(cube));
+    println!("cube hasn't been moved! :)");
+    println!("cube.to_string()\n{}", cube.to_string());
 
-        if count>10
-        {
-            println!("Breaking out of loop now");
-            break;
-        }
-    }
-    */
 }
